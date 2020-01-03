e0aeb10c93646bf80c64e31bb65f191ad4626557
Nestor-Anchundia <56656716+Nestor-Anchundia@users.noreply.github.com>
2020-01-03 00:42:51 -0500

    Add files via upload

---
+++ Clases/Casa1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using VentasDeCasa.Interfaz;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VentasDeCasa
+{
+    class Casa1: ICasas
+    {
+        public void pisos()
+        {
+            Console.WriteLine("La casa es de un solo piso");
+        }
+        public void cuartos()
+        {
+            Console.WriteLine("La casa cuenta con 2 Habitaciones");
+        }
+        public void baños()
+        {
+            Console.WriteLine("En toda la casa hay un solo Baño");
+        }
+        public void cocina()
+        {
+            Console.WriteLine("En la casa encontrara una pequeña  Cocina");
+        }
+        public void garage()
+        {
+            Console.WriteLine("La casa no cuenta con garage");
+        }
+
+    }
+}
---
+++ Clases/Casa5.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using VentasDeCasa.Interfaz;
+
+namespace VentasDeCasa.Clases
+{
+    class Casa5:ICasas
+    {
+        public void pisos()
+        {
+            Console.WriteLine("La casa es de uj piso");
+        }
+        public void cuartos()
+        {
+            Console.WriteLine("La casa cuenta con 1 Habitaciones");
+        }
+        public void baños()
+        {
+            Console.WriteLine("En toda la casa hay un Baño   ");
+
+        }
+        public void cocina()
+        {
+            Console.WriteLine("En la casa encontrara una  Cocina  ");
+        }
+        public void garage()
+        {
+            Console.WriteLine("No Cuenta con garage");
+        }
+    }
+}
